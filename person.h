20f71c31d928426b0f4a7f912370cff218df04ab
fafa1duo <100189371+fafa1duo@users.noreply.github.com>
2023-06-22 18:47:11 +0300

    Create source file person.h

---
+++ person.h
@@ -0,0 +1,19 @@
+#ifndef PESON_H
+#define PERSON_H
+
+struct person_address{
+char *street;
+char *state;
+char *city;
+}
+
+struct personDate{
+char *person_name;
+int person_id;
+int person_age;
+struct person_adderss personAdderss;
+}
+
+void personFunction(struct personDate date);
+
+#endif 
